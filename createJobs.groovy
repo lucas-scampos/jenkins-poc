b378cc61f2d78963af8f558df2efe7f252f21b78
lucascampos <Lucas_Campos@gap.com>
2020-08-07 09:10:22 -0300

    [Lucas Campos] - adding credentials

--- createJobs.groovy
+++ createJobs.groovy
@@ -11,6 +11,8 @@ pipelineJob('gorila-job') {
         cpsScm {
             scm {
                 git {
+                    credentials('fa991df9-b352-4dd4-a34e-ae8bc362570f')
+
                     remote {
                         url 'https://github.com/jpozorio/gorila-hackathon.git'
                     }
