ba3a6884025cb6401bc6129621f6009123f8614b
lucascampos <Lucas_Campos@gap.com>
2020-08-07 09:24:20 -0300

    [Lucas Campos] - reverting cred

--- createJobs.groovy
+++ createJobs.groovy
@@ -11,8 +11,6 @@ pipelineJob('gorila-job') {
         cpsScm {
             scm {
                 git {
-                    credentials('fa991df9-b352-4dd4-a34e-ae8bc362570f')
-
                     remote {
                         url 'https://github.com/jpozorio/gorila-hackathon.git'
                     }
